8a024e18145329b5cda07f9c0777774e2a95156e
Lord-Asterius <loic.mathiot70@gmail.com>
2020-09-24 23:46:22 +0200

    Added basic dispatch and get sanitization to index.php

--- index.php
+++ index.php
@@ -3,5 +3,25 @@
 include_once("src/views/HelloWorld.php");
 
 
-$helloWorldView = new HelloWorld();
-$helloWorldView->render();
+// We do a first sanitization pass by removing HTML tags and characters outside of the ASCII table
+$sanitizedGet = [];
+foreach ($_GET as $key => $value)
+{
+    $sanitizedGet[$key] = filter_var($value, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+}
+
+
+// Retrieve the requested page, Hello world is set as the default page for now
+$requestedPage = "helloWorld";
+if (isset($sanitizedGet["page"]))
+{
+    $requestedPage = $sanitizedGet["page"];
+}
+
+// Dispatch to controllers
+if ($requestedPage === "helloWorld")
+{
+    // Hello world is a dummy page so no controller is needed
+    $helloWorldView = new HelloWorld();
+    $helloWorldView->render();
+}
