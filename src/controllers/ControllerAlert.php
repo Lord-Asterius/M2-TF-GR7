65e00d4164b0df58ed629ebf30b5fe9135814b19
MathildeSiegwart <mathildesiegwart@hotmail.fr>
2020-09-29 19:32:12 +0200

    Alert page correction

--- src/controllers/ControllerAlert.php
+++ src/controllers/ControllerAlert.php
@@ -13,7 +13,7 @@ class ControllerAlert
 
     public function handleRequest($getParameters)
     {
-        $this->m_viewAlert->setAlertList([["Fabien Perreux", "3", "1"], ["Fabrice Bouquet", "4", "2"], ["Julien Bernard", "5", "3"]]);
+        $this->m_viewAlert->setAlertList([["21300598", "Fabien Perreux", "3", "1"], ["21300599", "Fabrice Bouquet", "4", "2"], ["21300597", "Julien Bernard", "5", "3"]]);
         $this->m_viewAlert->render();
     }
 }
