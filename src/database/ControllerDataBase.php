c3e849be463507940688b5371adefe1157a166d0
wernom <loic.sagona@gmail.com>
2020-10-06 14:00:21 +0200

    add select All Users

--- src/database/ControllerDataBase.php
+++ src/database/ControllerDataBase.php
@@ -118,7 +118,7 @@ class ControllerDataBase
     {
         if (self::$selectAllUser == null) {
             self::setPrepareToNull();
-            return self::$selectAllUser = self::$dataBaseConnector->prepare("SELECT * FROM user LEFT JOIN user_module ON user.key = user_module.user_key LEFT JOIN module ON user_module.module_key = module.key LEFT JOIN absence ON user.key = absence.etudiant_key");
+            return self::$selectAllUser = self::$dataBaseConnector->prepare("SELECT * FROM user");
         }
         return true;
     }
@@ -130,7 +130,6 @@ class ControllerDataBase
             return self::$selectAllTeacher = self::$dataBaseConnector->prepare("SELECT * FROM user LEFT JOIN user_module ON user.key = user_module.user_key LEFT JOIN module ON user_module.module_key = module.key WHERE user.role = 'ENSEIGNANT' ");
         }
         return true;
-
     }
 
     public static function prepareSelectAllAdminStaff()
@@ -160,7 +159,6 @@ class ControllerDataBase
             return self::$selectAllAllStudent = self::$dataBaseConnector->prepare("SELECT * FROM user LEFT JOIN user_module ON user.key = user_module.user_key LEFT JOIN module ON user_module.module_key = module.key LEFT JOIN absence ON user.key = absence.etudiant_key WHERE user.role = 'ETUDIANT' ");
         }
         return true;
-
     }
 
     public static function prepareSelectAllStudentInModule()
