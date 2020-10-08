8171ca63af3f055e2d9974309e2c095a3ec14c57
khadija15 <moustianekhadija@gmail.com>
2020-10-08 07:20:28 +0200

    Update PageIdentifiers.php

--- src/globals/PageIdentifiers.php
+++ src/globals/PageIdentifiers.php
@@ -5,6 +5,7 @@ define("PAGE_ID_MODULE_LIST", "moduleList");
 define("PAGE_ID_CONNECTION", "connection");
 define('PAGE_ID_DISCONNECTION', "disconnection");
 define("PAGE_ID_MODULE", "module");
+
 define("PAGE_ID_ABSENCE_LIST", "absenceList");
 define('PAGE_ID_ABSENSE_DETAIL',"absenceDetails");
 define('PAGE_ID_ADD_ABSENSE_DETAIL',"AddNewAbsence");
@@ -12,6 +13,10 @@ define('AJAX_LOAD_ABSENCE_DETAILS',"load_absence_details");
 define('AJAX_ADD_ABSENCE_DETAILS',"add_absence_details");
 define('AJAX_EDIT_ABSENCE_DETAILS',"edit_absence_details");
 define('AJAX_DELETE_ABSENCE_DETAILS',"delete_absence_details");
+
+define('DELETE_ABSENCE_STUDENT',"delete_student");
+define("PAGE_ID_ABSENCE_ADD", "absenceAdd");
+
 define('PAGE_ID_ALERT',"alert");
 // renommé avec la meme normalisation de nommage 
 define("PAGE_ID_ETUDIANT_LIST", "etudiantList");
@@ -37,11 +42,11 @@ define('MODIFY_ADMIN_ETUDIANT',"modifyAdminEtudiant");
 define('IS_RELEASE', false);
 
 //DataBase
-define('SERVER', '127.0.0.1');
-define('SERVER_RELEASE', 'm2gl.deptinfo-st.univ-fcomte.fr/phpmyadmin');
+define('SERVER', 'localhost');
+define('SERVER_RELEASE', 'localhost:8080/phpmyadmin');
 define('DB', 'qui_est_la');
 define('USER', 'root');
-define('USER_RELEASE', 'm2test7');
-define('PASS', 'password');
-define('PASS_RELEASE', 'm2test7');
+define('USER_RELEASE', '');
+define('PASS', '');
+define('PASS_RELEASE', '');
 define('PORT', '8080');
