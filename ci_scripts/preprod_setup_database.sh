0cfa718e6c761ae8147306072eec6fae97b044a4
Lord-Asterius <loic.mathiot70@gmail.com>
2020-10-13 17:26:29 +0200

    Changed preprod CI script

--- ci_scripts/preprod_setup_database.sh
+++ ci_scripts/preprod_setup_database.sh
@@ -1,4 +1,4 @@
-echo "DROP DATABASE IF EXISTS `preprod_qui_est_la`;" | mysql -u m2test7 -pm2test7 -h 172.20.128.68
-echo "CREATE DATABASE IF NOT EXISTS `preprod_qui_est_la` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;" | mysql -u m2test7 -pm2test7 -h 172.20.128.68
+echo "DROP DATABASE IF EXISTS preprod_qui_est_la;" | mysql -u m2test7 -pm2test7 -h 172.20.128.68
+echo "CREATE DATABASE IF NOT EXISTS preprod_qui_est_la DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;" | mysql -u m2test7 -pm2test7 -h 172.20.128.68
 mysql -u m2test7 -pm2test7 -h 172.20.128.68 preprod_qui_est_la < ./docker/preprod_qui_est_la.sql
 mv ./src/globals/Credentials.preprod.php ./src/globals/Credentials.php
