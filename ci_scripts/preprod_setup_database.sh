ed825e37e9be91bf484910259eb5b5a7d683496a
Lord-Asterius <loic.mathiot70@gmail.com>
2020-10-13 16:38:01 +0200

    Changed preprod database setup script

--- ci_scripts/preprod_setup_database.sh
+++ ci_scripts/preprod_setup_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -u m2test7 -pm2test7 -h 172.20.128.68 < ./docker/preprod_qui_est_la.sql
+cat ./docker/preprod_qui_est_la.sql | mysql -u m2test7 -pm2test7 -h 172.20.128.68
 mv ./src/globals/Credentials.preprod.php ./src/globals/Credentials.php
