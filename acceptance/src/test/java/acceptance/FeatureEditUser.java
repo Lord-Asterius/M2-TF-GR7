a65584ccaa4300585e8dcb57b343cae91cd8a798
Lord-Asterius <loic.mathiot70@gmail.com>
2020-10-14 22:10:26 +0200

    Added acceptances tests for edit teacher feature

--- acceptance/src/test/java/acceptance/FeatureEditUser.java
+++ acceptance/src/test/java/acceptance/FeatureEditUser.java
@@ -13,14 +13,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FeatureEditStudent
+public class FeatureEditUser
 {
     private HtmlUnitDriver m_driver;
     private Map<String, Object> m_vars;
     private JavascriptExecutor m_js;
 
-    private String m_studentFirstName;
-    private String m_studentLastName;
+    private String m_firstName;
+    private String m_lastName;
 
     @Given("^The administrator is connected$")
     public void theAdministratorIsConnected()
@@ -38,8 +38,17 @@ public class FeatureEditStudent
     {
         m_driver.findElement(By.linkText("Etudiants")).click();
         m_driver.findElement(By.cssSelector(".list-group-item:nth-child(1) > a")).click();
-        m_studentFirstName = m_driver.findElement(By.name("first_name")).getAttribute("value");
-        m_studentLastName = m_driver.findElement(By.name("last_name")).getAttribute("value");
+        m_firstName = m_driver.findElement(By.name("first_name")).getAttribute("value");
+        m_lastName = m_driver.findElement(By.name("last_name")).getAttribute("value");
+    }
+
+    @And("^The administrator is on a teacher modification page$")
+    public void theAdministratorIsOnATeacherModificationPage()
+    {
+        m_driver.findElement(By.linkText("Enseignants")).click();
+        m_driver.findElement(By.cssSelector(".list-group-item:nth-child(1) > a")).click();
+        m_firstName = m_driver.findElement(By.name("first_name")).getAttribute("value");
+        m_lastName = m_driver.findElement(By.name("last_name")).getAttribute("value");
     }
 
     @When("^The administrator change the name with \"([^\"]*)\"$")
@@ -48,26 +57,23 @@ public class FeatureEditStudent
         m_driver.findElement(By.name("first_name")).click();
         m_driver.findElement(By.name("first_name")).clear();
         m_driver.findElement(By.name("first_name")).sendKeys(name);
-        m_driver.findElement(By.id("exampleInputPassword1")).click();
-        m_driver.findElement(By.id("exampleInputPassword1")).clear();
-        m_driver.findElement(By.id("exampleInputPassword1")).sendKeys(" ");
         m_driver.findElement(By.cssSelector(".btn-primary")).click();
     }
 
-    @Then("^The new student name must be \"([^\"]*)\"$")
-    public void theNewStudentNameMustBe(String name)
+    @Then("^The new first name must be \"([^\"]*)\"$")
+    public void theNewFirstNameMustBe(String name)
     {
         {
-            List<WebElement> elements = m_driver.findElements(By.linkText(name + " " + m_studentLastName));
+            List<WebElement> elements = m_driver.findElements(By.linkText(name + " " + m_lastName));
             assert(elements.size() > 0);
         }
     }
 
-    @Then("^The student first name shouldn't has changed because empty first name aren't allowed$")
-    public void theStudentFirstNameShouldnTHasChangedBecauseEmptyFirstNameArenTAllowed()
+    @Then("^The first name shouldn't has changed because empty first name aren't allowed$")
+    public void theFirstNameShouldnTHasChangedBecauseEmptyFirstNameArenTAllowed()
     {
         {
-            List<WebElement> elements = m_driver.findElements(By.linkText(m_studentFirstName + " " + m_studentLastName));
+            List<WebElement> elements = m_driver.findElements(By.linkText(m_firstName + " " + m_lastName));
             assert(elements.size() > 0);
         }
     }
--- acceptance/src/test/resources/EditTeacher.feature
+++ acceptance/src/test/resources/EditTeacher.feature
@@ -1,12 +1,12 @@
-Feature: Edit Student
+Feature: Edit Teacher
   Scenario: Successful modification
     Given The administrator is connected
-    And The administrator is on a student modification page
-    When The administrator change the name with "Jean"
-    Then The new first name must be "Jean"
+    And The administrator is on a teacher modification page
+    When The administrator change the name with "Roger"
+    Then The new first name must be "Roger"
 
   Scenario: Modification error
     Given The administrator is connected
-    And The administrator is on a student modification page
+    And The administrator is on a teacher modification page
     When The administrator change the name with " "
     Then The first name shouldn't has changed because empty first name aren't allowed
